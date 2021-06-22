d56f6e896d0a19b79ba834abd5cc2bb553917a10
Anson Mau <ansonmau172@gmail.com>
2021-06-21 20:17:29 -0400

    Added a function that can print out a list with a given order.

--- mundane.py
+++ mundane.py
@@ -1,9 +1,21 @@
-def printList(l, num = -1):
+def printList(l, num = -1, order = []):
     if num == -1:
-        for i in range(len(l)):
-            print(f"[{i}]->{l[i]}")
-        return 
-    
-    for i in range(0,num):
+        if order:
+            myRange = order
+        else:
+            myRange = range(len(l))
+    else:
+        if order:
+            myRange = order[:num]
+        else:
+            myRange = range(0, num)
+
+    for i in myRange:
         print(f"[{i}]->{l[i]}")
     return
+
+def printListOrdered(l, order):
+    x = 0
+    for i in order:
+        print(f"[{x}]->{l[i]}")
+        x+=1
